56675c18a14c73df685f6c7eb1553d3b3a883452
George Henrique Wurthmann <gemakai@gmail.com>
2020-03-13 00:51:10 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SelectSearchComponent } from './select-search/select-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/select-search/select-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-search',
+  templateUrl: './select-search.component.html',
+  styleUrls: ['./select-search.component.css']
+})
+export class SelectSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
